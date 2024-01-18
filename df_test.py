0a608562d8cde027c15268b7567ea2fe66edbf47
Howard <118849685+Changsoo-Jang@users.noreply.github.com>
2024-01-19 00:13:23 +0900

    Add files via upload

--- df_test.py
+++ df_test.py
@@ -1,15 +1,58 @@
+# -*- coding:utf-8 -*-
 import os
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib import font_manager as fm 
+# from matplotlib import font_manager as fm 
+# import seaborn as sns
+# import numpy as np 
 
-## 한글 폰트 설정
-fpath = os.path.join(os.getcwd(), 'Nanum_Gothic/NanumGothic-Bold.ttf')
-prop = fm.FontProperties(fname=fpath)
+##################################################################
+
+# def unique(list):
+#     x = np.array(list)
+#     return np.unique(x)
+
+# @st.cache_data
+# def fontRegistered():
+#     font_dirs = [os.getcwd() + '/zzzzn']
+#     font_files = fm.findSystemFonts(fontpaths=font_dirs)
+
+#     for font_file in font_files:
+#         fm.fontManager.addfont(font_file)
+#     fm._load_fontmanager(try_read_cache=False)
+    
+
+# def main():
+    
+#     fontRegistered()
+#     fontNames = [f.name for f in fm.fontManager.ttflist]
+#     fontname = st.selectbox("폰트 선택", unique(fontNames))
+
+#     plt.rc('font', family=fontname)
+#     tips = sns.load_dataset("tips")
+#     fig, ax = plt.subplots()
+#     sns.scatterplot(data=tips, x = 'total_bill', y = 'tip', hue='day')
+#     ax.set_title("한글 테스트")
+#     st.pyplot(fig)
+    
+#     st.dataframe(tips)
+    
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+#####################################
+# ## 한글 폰트 설정
+# fpath = os.path.join(os.getcwd(), 'Nanum_Gothic/NanumGothic-Bold.ttf')
+# prop = fm.FontProperties(fname=fpath)
 plt.rcParams['font.family'] = "NanumGothic"
 plt.rcParams['axes.unicode_minus'] = False
 
+########################################################################
 uni_m_f_path = r"4년제일반대학교_등록금_좌표.xlsx"
 uni_m_df = pd.read_excel(uni_m_f_path,engine='openpyxl', header=0)
 uni_m_df.columns = ['학교명', '주소', 'x', 'y','사이트','등록금액','등급','심볼주소']
