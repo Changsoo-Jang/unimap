09721f82de6180114a6c0d8cd6e7c5fde68a273c
Changsoo-Jang <uuu1215@naver.com>
2024-01-20 11:05:40 +0900

    for test

--- df_test.py
+++ df_test.py
@@ -58,11 +58,11 @@ from matplotlib import font_manager as fm
 
 #####################################
 # ## 한글 폰트 설정
-fpath = os.path.join(os.getcwd(), 'NanumGothic.ttf')
-prop = fm.FontProperties(fname=fpath)
+# fpath = os.path.join(os.getcwd(), 'NanumGothic.ttf')
+# prop = fm.FontProperties(fname=fpath)
 plt.rcParams["font.family"] = 'NanumGothic'
 plt.rcParams['axes.unicode_minus'] = False
-# print(plt.rcParams["font.family"])
+# print(plt.rcParams["font.family"])ds
 ########################################################################
 uni_m_f_path = r"4년제일반대학교_등록금_좌표.xlsx"
 uni_m_df = pd.read_excel(uni_m_f_path,engine='openpyxl', header=0)
@@ -71,7 +71,7 @@ filtered_df = uni_m_df
 data_df = filtered_df[['학교명','등급','주소']]
 uName_list = filtered_df['학교명'].to_list()
 
-st.title(' 전국 4년제 대학 클래스 :red[2024년] 기준 ')
+st.title('전국 4년제 대학 레벨 :red[2024년] 기준')
 st.dataframe(data_df, use_container_width=True)
 
 fig, ax = plt.subplots()
@@ -81,7 +81,7 @@ bars = ax.bar(data_df['등급'], data_df['학교명'])
 
 for i, bar in enumerate(bars):
     yval = bar.get_height()
-    ax.text(bar.get_x(), yval, s=data_df['학교명'][i] , va='bottom', color='RED', fontsize=9, rotation=45)
+    ax.text(bar.get_x(), yval, s=data_df['학교명'][i], va='bottom', color='RED', fontsize=9, rotation=45)
     # va: vertical alignment  # ha value for align; 'center', 'right', 'left'
 
 st.markdown('<h1 style="font-size:20px; text-align:center">4년제 일반대학교 클래스 등급별 학교명</h1>', unsafe_allow_html=True)
